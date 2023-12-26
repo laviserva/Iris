af8aef6a4150ae22b93bb6b1a77c86e1a6844b25
Hanamori <thundereme@gmail.com>
2023-12-26 16:03:38 -0700

    Receive checkbox from analyzer app

--- web/analyzer/views.py
+++ web/analyzer/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
+import json
 import io
+
+import matplotlib.pyplot as plt
+from django.shortcuts import render
+import seaborn as sns
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 matplotlib.use('Agg')  # Usa el backend 'Agg' para Matplotlib
 
@@ -25,3 +30,21 @@ def generate_plot(request):
 
     # Devuelve la imagen como respuesta HTTP
     return HttpResponse(buffer, content_type='image/png')
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def process_algorithms(request):
+    data = json.loads(request.body)
+    sort_algorithms = data.get('sortAlgorithm', [])
+    search_algorithms = data.get('searchAlgorithm', [])
+    parallel_algorithms = data.get('paralellAlgorithm', [])
+
+    # Procesa los datos aquí
+    print('Los algoritmos de ordenamiento seleccionados son:')
+    print(sort_algorithms)
+    print("Los algoritmos de búsqueda seleccionados son:")
+    print(search_algorithms)
+    print("Los algoritmos paralelos seleccionados son:")
+    print(parallel_algorithms)
+
+    return JsonResponse({'status': 'success'})
