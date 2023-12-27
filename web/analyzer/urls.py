88c79b557d1c74db6480e68104208bfc508bb576
Hanamori <thundereme@gmail.com>
2023-12-26 18:51:18 -0700

    añadir nueva imagen

--- web/analyzer/urls.py
+++ web/analyzer/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.analyzer, name='analyzer'),
     path('process_algorithms/', views.process_algorithms, name='process-algorithms'),
+    path('latest_plot/', views.latest_plot, name='latest-plot'),  # Agregar esta línea
 ]
